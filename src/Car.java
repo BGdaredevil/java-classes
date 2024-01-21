4e52e6b9fdb57fefb5f575a1e21dca61b6d4d0fb
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-01-21 14:31:34 +0200

    solve carClass + constructor overloads

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,16 @@
 public class Car {
-    private String model;
-    private String brand;
-    private int power;
+    private String model = "unknown";
+    private String brand = "unknown";
+    private int power = -1;
+
+    public Car(String model) {
+        this.model = model;
+    }
+
+    public Car(String model, String brand) {
+        this.model = model;
+        this.brand = brand;
+    }
 
     public Car(String model, String brand, int power) {
         this.brand = brand;
--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,18 @@ public class Main {
             System.out.println("get car data");
             String[] carData = sc.nextLine().split(" ");
 
-            carList.add(new Car(carData[0], carData[1], Integer.parseInt(carData[2])));
+            if (carData.length == 1) {
+                carList.add(new Car(carData[0]));
+            }
+
+            if (carData.length == 2) {
+                carList.add(new Car(carData[0], carData[1]));
+            }
+
+            if (carData.length == 3) {
+                carList.add(new Car(carData[0], carData[1], Integer.parseInt(carData[2])));
+            }
+
         }
 
         carList.forEach(Car::getInfo);
